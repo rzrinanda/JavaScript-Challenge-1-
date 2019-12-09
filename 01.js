b605e65f8d392ca389fd5a1dd75d815ee4a9933b
Rizal Zulfikar <rzrinanda@gmail.com>
2019-12-09 13:46:42 +0700

    Palindrome

--- 01.js
+++ 01.js
@@ -1,1 +1,18 @@
-git 
+//palindrome
+function palindrome(str) {
+//   var re = /[\W_]/g;
+//   var lowRegStr = str.toLowerCase().replace(re, "");
+  var lowRegStr = str.toLowerCase();
+  var reverseStr = lowRegStr
+    .split(" ")
+    .reverse()
+    .join(" ");
+//   return reverseStr === lowRegStr;
+  return reverseStr;
+}
+const string1 = "ibu ratna antar ubi";
+const string2 = "kasur ini rusak";
+const result1 = palindrome(string1).split(' ');
+const result2 = palindrome(string2).split(' ');
+console.log(result1);
+console.log(result2);
