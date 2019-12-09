73893227c45f54a2f69ac713852dde41a24c0acf
Rizal Zulfikar <rzrinanda@gmail.com>
2019-12-09 17:20:22 +0700

    Done

--- 03.js
+++ 03.js
@@ -7,5 +7,17 @@ Samsung Galaxy J7, Smartphone, 3549000
 DELL XPS 13, Laptop, 26799000
 Xiaomi Mi 6, Smartphone, 5399000
 LG V30 Plus, Smartphone, 10499000`;
-const [name, category, price] = csv.split(', ');
-console.log(typeof name);
+var arr = csv.split('\n'); 
+
+var jsonObj = [];
+var headers = arr[0].split(',');
+for(var i = 1; i < arr.length; i++) {
+  var data = arr[i].split(',');
+  var obj = {};
+  for(var j = 0; j < data.length; j++) {
+     obj[headers[j].trim()] = data[j].trim();
+  }
+  jsonObj.push(obj);
+}
+console.log(JSON.stringify(jsonObj));
+JSON.stringify(jsonObj);
